1c4fc5007a1bee71f6d34c970c6cdea7369aa762
quocnna <63178225+quocnna@users.noreply.github.com>
2022-04-19 21:39:09 +0700

    no message

---
+++ module4/_10_session_cookie/exercise/e1/cart_shop/src/main/java/com/example/cart_shop/CartShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.cart_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartShopApplication.class, args);
+    }
+
+}
---
+++ module4/_10_session_cookie/exercise/e1/cart_shop/src/test/java/com/example/cart_shop/CartShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cart_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartShopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
