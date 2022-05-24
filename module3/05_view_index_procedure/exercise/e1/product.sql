f28270aae30c77e6728bc32dbf1ce3600c1aa47c
Nguyen Ngoc Anh Quoc <quocnna@gmail.com>
2022-05-24 11:34:34 +0700

    no message

--- module3/05_view_index_procedure/exercise/e1/product.sql
+++ module3/05_view_index_procedure/exercise/e1/product.sql
@@ -1,3 +1,4 @@
+-- 1
 create table product(
 id int primary key not null auto_increment,
 code varchar(50),
@@ -8,7 +9,7 @@ description varchar(100),
 status tinyint
 );
 
-
+-- 2
 insert into product (code,name,price,amount,description,status) values
 ('code1','name1',1,1,'description1',0),
 ('code2','name2',2,2,'description1',0),
@@ -38,41 +39,40 @@ rename table test_view to product_view;
 drop view product_view;
 
 -- 5
-
 DELIMITER //
 CREATE PROCEDURE getAll()
 BEGIN
 	SELECT * from product;
-END //
-DELIMITER ;
+END // DELIMITER ;
+
 call getAll();
+
 
 DELIMITER //
 CREATE PROCEDURE add_product(pcode VARCHAR(50), pname VARCHAR(50), pprice DOUBLE)
 BEGIN
 INSERT INTO product (`code`, `name`, price) VALUE(pcode, pname, pprice);
-END //
-DELIMITER ;
+END // DELIMITER ;
+
 CALL add_product('AP', 'apple', 123);
 
-DELIMITER
-//
+DELIMITER //
 create procedure update_product_by_id(pid int,pcode varchar(50),pname varchar(50),pprice double, pamount int, pdescription varchar(100), pstatus tinyint)
 begin
 update product
 set name=pname,code=pcode,price=pprice,amount=pamount,description=pdescription,status=pstatus
 where id=pid;
 end
-//
-DELIMITER ;
+// DELIMITER ;
+
 call update_product_by_id(1,'editcode','editname',9,9,'editdescription',1);
 
-DELIMITER
-//
+
+DELIMITER //
 CREATE PROCEDURE delete_product_id(pid int)
 BEGIN
 DELETE FROM product WHERE id= pid;
 END
-//
-DELIMITER ;
+// DELIMITER ;
+
 CALL delete_product_id(1);
---
+++ module3/05_view_index_procedure/practice/p2/store_procedure.sql
@@ -0,0 +1,10 @@
+DELIMITER //
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+  SELECT * FROM customers;
+END //
+DELIMITER ;
+
+call findAllCustomers();
+
+DROP PROCEDURE IF EXISTS `findAllCustomers`;
