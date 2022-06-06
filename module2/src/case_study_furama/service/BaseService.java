e8b45606212a0f6c67eb12a6d70a1bc78ef8c24b
Nguyen Ngoc Anh Quoc <quocnna@gmail.com>
2022-06-06 16:53:32 +0700

    remove cs

--- module2/src/case_study_furama/service/BaseService.java
+++
@@ -1,8 +0,0 @@
-package case_study_furama.service;
-
-import java.util.List;
-
-public interface BaseService<T> {
-    void save(T t);
-    List<T> getAll();
-}
--- module2/src/case_study_furama/service/EmployeeService.java
+++
@@ -1,6 +0,0 @@
-package case_study_furama.service;
-
-import case_study_furama.model.Employee;
-
-public interface EmployeeService extends BaseService<Employee> {
-}
