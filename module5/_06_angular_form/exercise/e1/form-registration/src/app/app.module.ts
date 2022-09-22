63e69f50625bf26f21aeed6a61889168e2843409
Nguyen Ngoc Anh Quoc <quocnna@gmail.com>
2022-09-22 16:46:27 +0700

    m5

---
+++ module5/_06_angular_form/exercise/e1/form-registration/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { FormRegisterComponent } from './form-register/form-register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormRegisterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
